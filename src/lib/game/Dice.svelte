913ba7533df99b3e43512615e0a3c0008af14916
molarmanful <benjinpang@gmail.com>
2024-04-27 02:27:53 -0400

    payout

--- src/lib/game/Dice.svelte
+++ src/lib/game/Dice.svelte
@@ -87,10 +87,7 @@
                 .reduce((a, b) => a + b, 0)
               const won = sum == 7 || sum == 12
               if (won)
-                $user.money =
-                  BigInt($user.money) +
-                  (BigInt(value) * 7n) / BigInt(1 + checked) +
-                  ''
+                $user.money = BigInt($user.money) + BigInt(value) * 7n + ''
               alert(won ? 'YOU WON! Keep winning!' : 'You lost :( Try again!')
               rolling = false
             }
